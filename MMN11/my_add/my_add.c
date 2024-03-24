5ea99545c21479a0166b5b76a53c89089cc305e6
yotamlevit <yotam.levit@gmail.com>
2024-03-24 22:57:47 +0200

    Added documentation

--- MMN11/my_add/my_add.c
+++ MMN11/my_add/my_add.c
@@ -4,28 +4,42 @@
 #include "stdio.h"
 
 #define BYTE_SIZE 8
-#define SHIFT_RIGHT(a, c) a >> c
-#define SHIFT_LEFT(a, c) a << c
 
+/*
+ * This function make a *simple* binary addition.
+ * The binary addition is when you combine the 2 right
+ * bits and checks if there is a carry.
+ *
+ * The function iterates over all the bits using a for loop.
+ * To isolate the bit from the numbers function uses shifting.
+ *
+ * param a: (unsigned int) the first number for the addition
+ * param b: (unsigned int) the second number for the addition
+ *
+ * @return: (unsigned int) The sum of the two numbers
+ */
 unsigned int my_add(unsigned int a, unsigned int b)
 {
-    unsigned int sum = 0, carry = 0, result = 0;
+    unsigned int sum = 0, carry = 0, result = 0, bitA, bitB, i;
 
-    for (int i = 0; i < sizeof(int) * BYTE_SIZE;i++) {
+    for (i = 0; i < sizeof(int) * BYTE_SIZE;i++) {
+        bitA = (a >> i) & 1;
+        bitB = (b >> i) & 1;
+
         // Calculate the sum of bits and carry
-        sum = SHIFT_RIGHT(a, i) ^ SHIFT_RIGHT(b, i) ^ carry;
+        sum = bitA ^ bitB  ^ carry;
 
         // Update the result with the sum at the current bit position
-        result |= SHIFT_LEFT(sum, i);
+        result |= (sum << i);
 
         // Calculate the new carry
-        carry = (SHIFT_RIGHT(a, i) & SHIFT_RIGHT(b, i)) | (SHIFT_RIGHT(a, i) & carry) | (SHIFT_RIGHT(b, i) & carry);
+        carry = (bitA & bitB) | (bitA & carry) | (bitB & carry);
     }
 
     return result;
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
     unsigned int num1, num2, sum;
 
--- MMN11/my_suffix/my_suffix.c
+++ MMN11/my_suffix/my_suffix.c
@@ -1,3 +1,58 @@
 //
 // Created by Yotam Levit on 24/03/2024.
 //
+#include "stdio.h"
+
+#define END_OF_STRING = '\0'
+
+/*
+ * This function finds suffix in a string that start with a given char
+ * When the function finds the suffix it prints it and counts it
+
+ *
+ * The function iterates over the string using a for loop.
+ *
+ * param str: (char[]) the string to iterate over
+ * param c: (char) the char that start the suffix.
+ *
+ * @return: (int) the number of suffix in the string.
+ *          if a suffix is found the program prints it.
+ */
+int suffix(char str[], char c)
+{
+    int count = 0;
+    char* currChar;
+
+    for (currChar = str; *currChar != '\0'; currChar++)
+    {
+        if (*currChar == c)
+        {
+            printf("%s\n", currChar);
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    char c, str[128];
+    int suffixResult;
+
+    // Read a char
+    printf("Enter a char: ");
+    scanf("%s", &c);
+
+    // Read a string
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Run suffix the sum
+    suffixResult = suffix(str, c);
+
+    // Print the sum
+    printf("The number of suffix is: %d\n", suffixResult);
+
+    return 0;
+}
