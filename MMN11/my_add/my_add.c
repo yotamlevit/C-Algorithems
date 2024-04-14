b1669ae68ea0f251d8fcf6c203155ba43153df08
User <yotam.levit@gmail.com>
2024-04-14 16:28:12 +0300

    Change comment to c90

--- MMN11/my_add/my_add.c
+++ MMN11/my_add/my_add.c
@@ -1,23 +1,23 @@
-//
-// Created by Yotam Levit on 24/03/2024.
-//
+/*
+ Created by Yotam Levit on 24/03/2024.
+*/
 #include "stdio.h"
 
 #define BYTE_SIZE 8
 
-//
-// This function make a *simple* binary addition.
-// The binary addition is when you combine the 2 right
-// bits and checks if there is a carry.
-//
-// The function iterates over all the bits using a for loop.
-// To isolate the bit from the numbers function uses shifting.
-//
-// param a: (unsigned int) the first number for the addition
-// param b: (unsigned int) the second number for the addition
-//
-// @return: (unsigned int) The sum of the two numbers
-//
+/*
+ This function make a *simple* binary addition.
+ The binary addition is when you combine the 2 right
+ bits and checks if there is a carry.
+
+ The function iterates over all the bits using a for loop.
+ To isolate the bit from the numbers function uses shifting.
+
+ param a: (unsigned int) the first number for the addition
+ param b: (unsigned int) the second number for the addition
+
+ @return: (unsigned int) The sum of the two numbers
+*/
 unsigned int my_add(unsigned int a, unsigned int b)
 {
     unsigned int sum = 0, carry = 0, result = 0, bitA, bitB, i;
@@ -26,13 +26,13 @@ unsigned int my_add(unsigned int a, unsigned int b)
         bitA = (a >> i) & 1;
         bitB = (b >> i) & 1;
 
-        // Calculate the sum of bits and carry
+        /* Calculate the sum of bits and carry */
         sum = bitA ^ bitB  ^ carry;
 
-        // Update the result with the sum at the current bit position
+        /* Update the result with the sum at the current bit position */
         result |= (sum << i);
 
-        // Calculate the new carry
+        /* Calculate the new carry */
         carry = (bitA & bitB) | (bitA & carry) | (bitB & carry);
     }
 
@@ -43,17 +43,17 @@ int main()
 {
     unsigned int num1, num2, sum;
 
-    // Prompt user for input
+    /* Prompt user for input */
     printf("Enter two integers (example: '11 22'): ");
 
-    // Read two integers
+    /* Read two integers */
     scanf("%u %u", &num1, &num2);
 
     printf("Num1: %u, num2: %u", num1, num2);
-    // Calculate the sum
+    /* Calculate the sum */
     sum = my_add(num1, num2);
 
-    // Print the sum
+    /* Print the sum */
     printf("\nThe binary addition: %u + %u =  %u\n", num1, num2, sum);
 
     return 0;
--- MMN11/my_suffix/my_suffix.c
+++ MMN11/my_suffix/my_suffix.c
@@ -1,23 +1,23 @@
-//
-// Created by Yotam Levit on 24/03/2024.
-//
+/*
+ Created by Yotam Levit on 24/03/2024.
+*/
 #include "stdio.h"
 
 #define END_OF_STRING = '\0'
 
-//
-// This function finds suffix in a string that start with a given char
-// When the function finds the suffix it prints it and counts it
+/*
+ This function finds suffix in a string that start with a given char
+ When the function finds the suffix it prints it and counts it
 
-//
-// The function iterates over the string using a for loop.
-//
-// param str: (char[]) the string to iterate over
-// param c: (char) the char that start the suffix.
-//
-// @return: (int) the number of suffix in the string.
-//          if a suffix is found the program prints it.
-//
+
+ The function iterates over the string using a for loop.
+
+ param str: (char[]) the string to iterate over
+ param c: (char) the char that start the suffix.
+
+ @return: (int) the number of suffix in the string.
+          if a suffix is found the program prints it.
+*/
 int suffix(char str[], char c)
 {
     int count = 0;
@@ -40,19 +40,19 @@ int main()
     char c, str[128];
     int suffixResult;
 
-    // Read a char
+    /* Read a char */
     printf("Enter a char: ");
     scanf("%s", &c);
 
-    // Read a string
+    /* Read a string */
     printf("Enter a string: ");
     scanf("%s", str);
 
     printf("The given string is: %s \nThe given char is: %c\n", str, c);
-    // Run suffix the sum
+    /* Run suffix the sum */
     suffixResult = suffix(str, c);
 
-    // Print the sum
+    /* Print the sum */
     printf("The number of suffix is: %d\n", suffixResult);
 
     return 0;
