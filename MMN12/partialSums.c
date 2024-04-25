e73ef10d60373551ba4b9b62c9f5b1aa9d6a87a8
yotamlevit <yotam.levit@gmail.com>
2024-04-25 09:58:13 +0300

    Finished partial sums

--- MMN12/partialSums.c
+++ MMN12/partialSums.c
@@ -1,46 +1,87 @@
-//
-// Created by Yotam Levit on 24/03/2024.
-//
+/*
+  Created by Yotam Levit on 24/03/2024.
+*/
 
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int* partialSums(int arr[], int length)
+
+/*
+ This function calculates the partial sums of an array.
+ It iterates over the array, adding each element to a cumulative sum
+ and stores this cumulative sum in a new array at the corresponding index.
+
+ param arr: (int*) pointer to the array of integers
+ param length: (int) number of elements in the array
+
+ @return: (int*) pointer to the new array containing partial sums
+*/
+int* partialSums(int* arr, int length)
 {
-    //COde
+    int i, sum = 0;
+    int* partialSums = (int*)malloc(sizeof(int) * length);
+
+    for (i = 0; i < length; ++i) {
+        sum += arr[i];
+        partialSums[i] = sum;
+    }
+
+    return partialSums;
+
 }
 
+
+/*
+ This function retrieves an array of integers from the user input.
+ It prompts the user to enter integers one by one and stores them in a dynamically allocated array.
+
+ param length: (int) number of integers to be read
+
+ @return: (int*) pointer to the dynamically allocated array of integers
+*/
 int* getArrayInput(int length)
 {
     int i;
-    int arr[length];
+    int* arr = (int*)malloc(sizeof(int) * length);
+
 
     printf("Array length is %d\nEnter the first number: ", length);
     scanf("%d", arr);
 
     for (i = 1; i < length; i++) {
-        printf("Enter the next number: ");
+        printf("Enter the %d number: ", i+1);
         scanf("%d", arr + i);
     }
 
-    return (int*)arr;
+    return arr;
 }
 
+
+/*
+ This function prints an array of integers in a formatted manner.
+ It iterates over the array, printing each integer followed by a comma except for the last integer.
+
+ param arr: (int*) pointer to the array of integers
+ param length: (int) number of elements in the array
+*/
 void printArray(int* arr, int length)
 {
     int i;
 
-    printf("\n{ ");
+    printf("\n{");
     for (i = 0; i < length-1; ++i)
     {
-        printf(" %d, ", arr[i]);
+        printf("%d, ", arr[i]);
     }
 
-    printf("%d }\n", arr[length-1]);
+    printf("%d}\n", arr[length-1]);
 }
+
 
 int main() {
     int length;
-    int *pArr;
+    int* pArr;
+    int* pPartialSums;
 
     printf("Enter the array`s length: ");
     scanf("%d", &length);
@@ -49,5 +90,13 @@ int main() {
 
     printf("The given array (length=%d) is:", length);
     printArray(pArr, length);
+
+    pPartialSums = partialSums(pArr, length);
+
+    printf("The returned partial sums array (length=%d) is:", length);
+    printArray(pPartialSums, length);
+
+    free(pPartialSums);
+    free(pArr);
 }
 
